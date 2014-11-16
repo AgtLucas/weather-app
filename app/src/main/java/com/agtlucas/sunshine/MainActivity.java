bfd2fc0dd2717bb2b76250e2d95f6371e6917ee8
Lucas <lucas@agtlucas.com>
2014-11-16 11:54:11 -0200

    Update MainActivity

--- app/src/main/java/com/agtlucas/sunshine/MainActivity.java
+++ app/src/main/java/com/agtlucas/sunshine/MainActivity.java
@@ -78,8 +78,7 @@ public class MainActivity extends ActionBarActivity {
                     getActivity(),
                     R.layout.list_item_forecast,
                     R.id.list_item_forecast_textview,
-                    weekForecast
-            );
+                    weekForecast);
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
